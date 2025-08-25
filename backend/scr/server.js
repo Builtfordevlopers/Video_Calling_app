9818363b32e126f93665f7d86423e9a39569f5fe
Dipak Mehta <dipakmehta9804@gmail.com>
2025-08-25 18:52:05 +0530

    Db connected & env.js ready

--- backend/scr/server.js
+++ backend/scr/server.js
@@ -1,15 +1,16 @@
 import express from "express";
 import { ENV } from "./config/env.js";
+import { connect } from "mongoose";
+import { connectDB } from "./config/db.js";
 
 const app = express();
-
-
 
 app.get("/", (req, res)=>{
     res.send("Hello worls");
 });
 
-console.log("mongo_uri:", ENV.MONGO_URI);
 
-
-app.listen(ENV.PORT, ()=> console.log("Server started on port:", ENV.PORT));
+app.listen(ENV.PORT, ()=>{
+    console.log("Server started on port:", ENV.PORT);
+    connectDB();
+});

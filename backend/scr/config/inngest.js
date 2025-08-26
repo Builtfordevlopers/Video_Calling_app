2943a1940d212a6b08d2b24bb783f6fe184bb9b8
Dipak Mehta <dipakmehta9804@gmail.com>
2025-08-26 15:42:54 +0530

    vercel.json added

--- backend/scr/config/inngest.js
+++ backend/scr/config/inngest.js
@@ -3,7 +3,6 @@ import { connectDB } from "./db.js";
 import { User } from "../models/user.model.js"
 
 
-// Create a client to send and receive events
 export const inngest = new Inngest({ id: "Meet_Tanti" });
 
 const syncUser = inngest.createFunction(
